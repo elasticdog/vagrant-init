d6a306a63d2b087602ffb907534690d411ffdcc7
Aaron Bull Schaefer <aaron@elasticdog.com>
2011-12-22 15:43:52 -0800

    add cookbook metadata configuration via knife.rb

---
+++ .chef/knife.rb
@@ -0,0 +1,4 @@
+cookbook_copyright 'Aaron Bull Schaefer'
+cookbook_email     'aaron@elasticdog.com'
+cookbook_license   'mit'
+cookbook_path      [ File.expand_path('../cookbooks', File.dirname(__FILE__)) ]
