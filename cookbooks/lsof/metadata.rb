e475552368ed86932f4392746c5e775ec95f19ca
Aaron Bull Schaefer <aaron@elasticdog.com>
2011-12-27 11:11:20 -0800

    add lsof cookbook

---
+++ cookbooks/lsof/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Aaron Bull Schaefer"
+maintainer_email "aaron@elasticdog.com"
+license          "MIT"
+description      "Installs/Configures lsof"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
