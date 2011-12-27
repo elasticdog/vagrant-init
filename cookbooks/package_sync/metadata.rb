f8261ab41dcc9d50b41af0efadfaa5cf7aacaf52
Aaron Bull Schaefer <aaron@elasticdog.com>
2011-12-27 13:02:13 -0800

    Add package_sync cookbook
    This will sync the appropriate package database for the package manager
    (apt, pacman, or yum) that the system is using, and will install the
    epel repo when using yum.

--- cookbooks/package_sync/metadata.rb
+++ cookbooks/package_sync/metadata.rb
@@ -1,11 +1,6 @@
 maintainer       "Aaron Bull Schaefer"
 maintainer_email "aaron@elasticdog.com"
 license          "MIT"
-description      "Installs/Configures vagrant_main"
+description      "Installs/Configures package_sync"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-
-%w{ ack ctags curl dotfiles git htop lsof mercurial package_sync tmux tree
-    rbenv vim zsh }.each do |cookbook|
-  depends cookbook
-end
